eb4d7a473a29aeb5af125cfc769fbe1f090dc8b4
BlueHouse <BlueHouse>
2020-10-27 23:48:21 +0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Private Variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    void Update()
+    {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime *  speed * forwardInput);
+        // We turn  the vehicle
+        transform.Rotate(Vector3.up, turnSpeed *  horizontalInput * Time.deltaTime);
+    }
+
+}
